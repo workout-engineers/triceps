f14dacaffe92993f888ffb953512bbfe108f4c82
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-17 14:36:32 +0900

    tweets-table migration

--- db/migrate/20180617044256_create_tweets.rb
+++ db/migrate/20180617044256_create_tweets.rb
@@ -1,6 +1,10 @@
 class CreateTweets < ActiveRecord::Migration[5.1]
   def change
     create_table :tweets do |t|
+      t.text :quote, null: false
+      t.text :comment
+      t.integer :book_id
+      t.integer :user_id
 
       t.timestamps
     end
