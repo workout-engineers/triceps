8ab79545fcbc0a759dd09cad78e3172a70b67e01
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-18 17:31:28 +0900

    migrated favorite_authors-table

--- db/migrate/20180618082912_create_favorite_authors.rb
+++ db/migrate/20180618082912_create_favorite_authors.rb
@@ -1,6 +1,8 @@
 class CreateFavoriteAuthors < ActiveRecord::Migration[5.1]
   def change
     create_table :favorite_authors do |t|
+      t.integer :user_id, null:false
+      t.integer :author_id, null:false
 
       t.timestamps
     end
