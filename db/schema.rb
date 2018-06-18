e9c6dbcff1035020d1bda034444b6d8216a6d401
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-18 17:20:32 +0900

    created user-model and migrated users-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180617044256) do
+ActiveRecord::Schema.define(version: 20180617121823) do
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "quote", null: false
     t.text "comment"
     t.integer "book_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
