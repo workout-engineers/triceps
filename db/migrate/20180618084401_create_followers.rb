a6fb2cae71ebe06d28fca909263234aead8f44b3
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-18 17:46:46 +0900

    migrated followers-table

--- db/migrate/20180618084401_create_followers.rb
+++ db/migrate/20180618084401_create_followers.rb
@@ -2,7 +2,7 @@ class CreateFollowers < ActiveRecord::Migration[5.1]
   def change
     create_table :followers do |t|
       t.integer :user_id, null: false
-      t.integer :following_user_id, null: false
+      t.integer :followed_user_id, null: false
 
       t.timestamps
     end
