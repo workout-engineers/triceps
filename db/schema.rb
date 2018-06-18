fe06bdb2d72ba2c2dce5f7054244116035a44e4d
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-18 17:41:09 +0900

    created follow-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618083218) do
+ActiveRecord::Schema.define(version: 20180618083454) do
 
   create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.integer "author_id"
+    t.text "book_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
