97f3889ffb34e65a3ea7e78327c7210ddc15f385
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-18 17:28:38 +0900

    migrated favorite_books-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180617121823) do
+ActiveRecord::Schema.define(version: 20180618082429) do
+
+  create_table "favorite_books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "quote", null: false
