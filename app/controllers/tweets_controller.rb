36f40206a74f9c93d76436250fc4e501fcb3b83a
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-20 19:38:30 +0900

    write resources in routing file

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,28 @@
 class TweetsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  private
+  def tweet_params
+    params.permit()
+  end
+
 end
