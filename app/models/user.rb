3af0e25fe4bd4a655ae690f986f16eab1e3d20e9
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-25 13:50:58 +0900

    fix strong paramater in user-registraion

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: {message: '名前を入力してください'}
 end
