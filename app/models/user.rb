ff0dfb12867778b0ddf478364d37e6d195991726
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-25 19:44:28 +0900

    set email-comfirmation of user-registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :name, presence: {message: '名前を入力してください'}
   validates :agreement, presence: {message: "利用規約への同意が必要です。"}
 end
