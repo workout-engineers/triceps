36da55655c958072d04f5adabd5b427f5479c23e
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-06-25 13:48:13 +0900

    add strong paramater in user-registraion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :password, :password_confirmation, :name)}
+  end
 end
