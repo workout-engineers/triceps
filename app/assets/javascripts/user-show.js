e186eacc74da48448047d32e0479dd2099ec1e89
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-07-09 00:56:36 +0900

    line-fix user-show.js

--- app/assets/javascripts/user-show.js
+++ app/assets/javascripts/user-show.js
@@ -1,14 +1,16 @@
-
 $(function(){
   var user_info_scr = $(".user-info-scr");
   user_info_scr.hide();
+
   $(window).scroll(function(){
     var present_place = $(this).scrollTop();
     var chage_place = 170;
+
     if(present_place > chage_place){
       user_info_scr.fadeIn(80);
     } else {
       user_info_scr.fadeOut(80);
     }
+
   });
 });
