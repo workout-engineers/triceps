f38ea3e35b33f434669adc8bb5a9b125397c07de
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-07-12 11:02:45 +0900

    change order of tweet DESC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
  def show
    @user = User.find(params[:id])
-   @tweets = @user.tweets
+   @tweets = @user.tweets.order("created_at DESC")
  end
 
 end
