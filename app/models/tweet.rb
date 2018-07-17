9d87933db4c7823f46dfe10d326815794faf8273
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-07-17 13:04:20 +0900

    move correct_user action from controller to model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,13 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  def correct_user
+    @tweet = Tweet.find(params[:id])
+    if @tweet.user_id == current_user.id
+      return true
+    else
+      redirect_to user_path(current_user)
+    end
+  end
 end
+
