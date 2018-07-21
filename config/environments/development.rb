5f1aee57bac41f5f11195de3d9fbb1edfb92ac51
金正朋也 <mhtnim1023@gmail.com>
2018-07-22 00:58:32 +0900

    環境変数を利用

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,14 +32,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'smtp.gmail.com',
-    :user_name => Settings.gmail[:user_name], #gmailアドレス
-    :password => Settings.gmail[:password], #gmailパスワード
+    :user_name => ENV["GMAIL"] || Settings.gmail[:user_name], #gmailアドレス
+    :password => ENV["GMAIL_PASSWORD"] || Settings.gmail[:password], #gmailパスワード
     :authentication => 'login',
   }
 
