3799fe31e370cb4980a4f32ce180093a90cc3232
金正朋也 <mhtnim1023@gmail.com>
2018-07-22 01:41:41 +0900

    follow follwerのassociationを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   validates :agreement, presence: { message: '利用規約への同意が必要です。' }
 
   has_many :tweets, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'following_id', dependent: :destroy
+  has_many :followings, through: :active_relationships
+  has_many :followers, through: :passive_relationships
 end
