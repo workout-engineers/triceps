c4fc04d34f9b887e439bf3963e2b32a3815977e0
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-07-22 19:23:40 +0900

    relationshipsコントローラの編集

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,14 @@
 class RelationshipsController < ApplicationController
+  before_action :auth_user
+
+  def create
+    @user = User.find(params[:following_id])
+    current_user.follow(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:following_id])
+    current_user.unfollow(@user)
+  end
+
 end
