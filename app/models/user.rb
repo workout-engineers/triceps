f6d86a2a3ab3ebcf322e927715ae914b028f4d9a
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-07-22 16:17:23 +0900

    微調整と、seedの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,20 +8,20 @@ class User < ApplicationRecord
 
   has_many :tweets, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, \
-                                  inverse_of: :followings
+                                  inverse_of: :following
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'following_id', dependent: :destroy, \
-                                   inverse_of: :followers
+                                   inverse_of: :follower
   has_many :followings, through: :active_relationships
   has_many :followers, through: :passive_relationships
 
   # ユーザーをフォローする
   def follow(other_user)
-    active_relationships.create(followed_id: other_user.id)
+    active_relationships.create(following_id: other_user.id)
   end
 
   # ユーザーをフォロー解除する
   def unfollow(other_user)
-    active_relationships.find_by(followed_id: other_user.id).destroy
+    active_relationships.find_by(following_id: other_user.id).destroy
   end
 
 
