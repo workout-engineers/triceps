5722a4dbfed341dc6c3a44f08313c21c3a28baa5
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-07-23 15:15:50 +0900

    ユーザー詳細ページのフォロー機能実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,14 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:following_id])
     current_user.follow(@user)
+    redirect_to @user
   end
 
   def destroy
     unfollow_user_id = Relationship.find(params[:id]).following_id
     @user = User.find(unfollow_user_id)
     current_user.unfollow(@user)
+    redirect_to @user
   end
 
 end
