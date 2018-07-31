2cdd22083d40bbf56fe7427371efba582ec965b9
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-07-31 23:43:24 +0900

    フォロー/アンフォローボタンの編集

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,10 +8,8 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    unfollow_user_id = Relationship.find(params[:id]).following_id
-    @user = User.find(unfollow_user_id)
+    @user = User.find(params[:following_id])
     current_user.unfollow(@user)
     redirect_to @user
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,24 @@
 class UsersController < ApplicationController
+before_action :set_user
 
   def followings
     @title = "がフォロー中"
-    @user = User.find(params[:id])
     @users = @user.followings
     render "show_follow"
   end
 
   def followers
     @title = "のフォロワー"
-    @user = User.find(params[:id])
     @users = @user.followers
     render "show_follow"
   end
 
   def show
-    @user = User.find(params[:id])
     @tweets = @user.tweets.order('created_at DESC')
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
